3a60c1d9f384a29abc4fb2b72a9b9c73f4efd77f
BathinaPavani <102909176+BathinaPavani@users.noreply.github.com>
2026-01-29 13:27:50 -0600

    main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,24 @@
 provider "aws" {
-    region="us-east-1"
+    region     = "us-east-1"
+    profile    = "default"
 }
-resource "aws_instance""EC2_1inctest"{
-    ami = "ami-0c02fb55956c7d316"
+
+resource "aws_instance" "ec2_instance_pro_test" {
+    ami           = "ami-0c02fb55956c7d316"
     instance_type = "t2.micro"
-    tags={
-        Name="EC2_1inctest"
-        project="Terraform"    }
+    tags = {
+        Name    = "pro_test_instance"
+        project = "pro_test_project"
+    }
 }
-resource "aws_sns_topic" "ec2_start_stop_topic" {
-    name = "ec2-start-stop-topic"
+
+resource "aws_sns_topic" "ec2_sns_alerts" {
+    name = "ec2-sns-alerts"
 }
-//optional   
-resource "aws_iam_role" "lamda_role"{
-    name = "lambda_ec2_start_stop_role"
+
+// optional
+resource "aws_iam_role" "lambda_iam_role" {
+    name = "lambda_iam_role"
     assume_role_policy = jsonencode({
         Version = "2012-10-17"
         Statement = [
@@ -23,17 +28,20 @@ resource "aws_iam_role" "lamda_role"{
                 Principal = {
                     Service = "lambda.amazonaws.com"
                 }
-            },
+            }
         ]
     })
 }
-resource "aws_lambda_function" "ec2_example"{
-    function_name ="example_lambda"
-    runtime = "python3.9"
-    handler="lambda_function.lambda_handler"
-    role = aws_iam_role.lamda_role.arn 
-    filename ="lambda.zip"
+
+resource "aws_lambda_function" "example" {
+    function_name    = "example_lambda"
+    runtime          = "python3.9"
+    handler          = "lambda_function.lambda_handler"
+    role             = aws_iam_role.lambda_iam_role.arn
+    filename         = "lambda.zip"
     source_code_hash = filebase64sha256("lambda.zip")
-    timeout =10
-    memory_size =128 
-    }
+    timeout          = 10
+    memory_size      = 128
+}
+
+
